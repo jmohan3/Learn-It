11ed21fb8f056ee6a7ff2322051f0cddf6330405
Jagan <jaganmohan.ios3@gmail.com>
2025-07-07 10:57:24 +0530

    setting up X-Code project

---
+++ Learn-It/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learn-It
+//
+//  Created by Kudumala on 07/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learn-It/Learn_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_ItApp.swift
+//  Learn-It
+//
+//  Created by Kudumala on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Learn_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
